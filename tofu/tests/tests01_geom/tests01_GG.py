1a24fa02ef9f372e455874c7583712c83ba3649b
Laura Mendoza <mendoza@math.unistra.fr>
2019-10-22 13:56:28 +0200

    [bugfix] fixed return dimension for user func in tuto

--- tofu/tests/tests01_geom/tests01_GG.py
+++ tofu/tests/tests01_geom/tests01_GG.py
@@ -7,7 +7,6 @@ import os
 import sys
 import numpy as np
 import matplotlib
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.gridspec as mplgrid
